1be5741628873ac358a66cec6fef43390aa596a7
sugiyama404 <mmingriha@gmail.com>
2023-08-05 18:19:04 +0900

    ok

--- webserver/myapp/next.config.js
+++ webserver/myapp/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        APSERVER_ADDRESS: process.env.APSERVER_ADDRESS,
+    },
     experimental: {
         serverActions: true,
     },
