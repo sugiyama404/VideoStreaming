12e4d96f03fb3030312f3a12f5beb5e1f4a0d87b
sugiyama404 <mmingriha@gmail.com>
2023-08-06 16:50:03 +0900

    save point

--- webserver/myapp/app/api/auth/[...nextauth]/options.ts
+++ webserver/myapp/app/api/auth/[...nextauth]/options.ts
@@ -2,6 +2,12 @@
 import type { NextAuthOptions } from 'next-auth'
 //@ts-ignore
 import CredentialsProvider from 'next-auth/providers/credentials'
+//@ts-ignore
+import * as grpc from '@grpc/grpc-js';
+import { ProfileClient } from '@/types/pb/user/user_grpc_pb';
+import { AuthUserRequest, AuthUserReply } from '@/types/pb/user/user_pb';
+//@ts-ignore
+const target: string = process.env.APSERVER_ADDRESS;
 
 export const options: NextAuthOptions = {
     providers: [
@@ -45,5 +51,30 @@ export const options: NextAuthOptions = {
             if (session?.user) session.user.role = token.role
             return session
         },
+    }
+}
+
+async function userauth(email: string, password: string) {
+    const client = new ProfileClient(target, grpc.credentials.createInsecure());
+    const req = new AuthUserRequest();
+    req.setEmail(email);
+    req.setPassword(password);
+    const res: AuthUserReply = await new Promise((resolve, reject) => {
+        client.authUser(req, (err: any, res: AuthUserReply) => {
+            if (err) {
+                console.log(err)
+                const rep = new AuthUserReply();
+                rep.setIsuser(false);
+                reject(rep)
+            } else {
+                resolve(res)
+            }
+        });
+    });
+
+    if (!res.getIsuser()) {
+        return (res.getIsuser(), null)
+    } else {
+        return (res.getIsuser(), res.getInfo)
     }
 }
