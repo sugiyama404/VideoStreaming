d73663c2ee65e4ee853fd4a08f1ed7b339427a43
sugiyama404 <mmingriha@gmail.com>
2023-08-17 13:54:30 +0900

    tmp saved

--- webserver/myapp/app/administrator/adminsidebar.tsx
+++ webserver/myapp/app/administrator/adminsidebar.tsx
@@ -8,6 +8,7 @@ export default function Adminsidebar() {
       <ul className="uk-list">
         <li><Link className="uk-link-text" href="/administrator">動画ダッシュボード</Link></li>
         <li><Link className="uk-link-text" href="/administrator/upload">動画アップロード</Link></li>
+        <li><Link className="uk-link-text" href="/administrator/videodetail?name=FFF9180A3A7211EEBF7C0242AC170005">動画詳細</Link></li>
       </ul>
     </div>
   )
