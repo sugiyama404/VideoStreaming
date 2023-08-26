8b0b998dd0bd7a5eb9bd69fce8d13698ade650ba
sugiyama404 <mmingriha@gmail.com>
2023-08-26 18:11:21 +0900

    make video list

--- webserver/myapp/app/administrator/adminsidebar.tsx
+++ webserver/myapp/app/administrator/adminsidebar.tsx
@@ -8,7 +8,6 @@ export default function Adminsidebar() {
       <ul className="uk-list">
         <li><Link className="uk-link-text" href="/administrator">動画ダッシュボード</Link></li>
         <li><Link className="uk-link-text" href="/administrator/upload">動画アップロード</Link></li>
-        <li><Link className="uk-link-text" href="/administrator/videodetail?name=305c1791-4237-11ee-adf1-0242ac170005">動画詳細</Link></li>
       </ul>
     </div>
   )
--- webserver/myapp/app/administrator/page.tsx
+++ webserver/myapp/app/administrator/page.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
+import { Videolist } from '@/types/typing/video';
+
+async function fetchData() {
+  const endpoint = 'http://localhost/api/video/list'
+  const res = await fetch(endpoint, {
+    cache: "no-store",
+  });
+  const data: Videolist[] = await res.json();
+  return data
+}
+
+export default async function Home() {
+  const data: Videolist[] = await fetchData();
   return (
     <div>
-      <h1>administrator</h1>
+      <h1>動画ダッシュボード</h1>
+      <h2>動画一覧</h2>
+      <table className="uk-table uk-table-middle uk-table-divider">
+        <thead>
+          <tr>
+            <th>タイトル</th>
+            <th>説明</th>
+            <th>タグ</th>
+            <th>カテゴリー</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((v) => (
+            <tr key={v.id}>
+              <td>{v.title}</td>
+              <td>{v.explain}</td>
+              <td>{v.tags.join(',')}</td>
+              <td>{v.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
