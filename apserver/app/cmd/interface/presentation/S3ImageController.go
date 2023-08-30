4c24fbb53e82420fc919e6029eb1e2ad7a6d4a08
sugiyama404 <mmingriha@gmail.com>
2023-08-30 16:09:45 +0900

    tmp saverd

--- apserver/app/cmd/interface/presentation/S3ImageController.go
+++ apserver/app/cmd/interface/presentation/S3ImageController.go
@@ -4,12 +4,17 @@ import (
 	pb "app/cmd/domain/pb/s3image"
 	"app/cmd/interface/repository"
 	"app/cmd/usecase"
+	"bytes"
 	"context"
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"os"
 
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/nfnt/resize"
 )
 
 type S3ImageServer struct {
@@ -90,11 +95,40 @@ func (s *S3ImageServer) ImageStreamUpload(stream pb.Imagetransporter_ImageStream
 	return stream.SendAndClose(&pb.Message{Message: "success"})
 }
 
-func (s *S3ImageServer) ImageDownload(ctx context.Context, in *pb.ImageDownloadRequest) (*pb.ImageDownloadResponse, error) {
+func (s *S3ImageServer) ImageDownload(ctx context.Context, in *pb.ImageDownloadRequest) (*pb.ImageDownloadReplay, error) {
 	res, err := s.Interactor.ImageDownload(in.GetName())
 	if err != nil {
 		return nil, err
 	}
-	res_c := res.Bytes()
-	return &pb.ImageDownloadResponse{Image: res_c}, err
+	var res_c []byte
+	if in.GetIsresize() {
+		fmt.Println("resize")
+		img, data, err := image.Decode(res)
+		if err != nil {
+			return nil, err
+		}
+
+		resizedImg := resize.Resize(uint(in.GetRewidth()), uint(in.GetReheight()), img, resize.NearestNeighbor)
+		buf := new(bytes.Buffer)
+		switch data {
+		case "png":
+			if err := png.Encode(buf, resizedImg); err != nil {
+				return nil, err
+			}
+		case "jpeg", "jpg":
+			opts := &jpeg.Options{Quality: 100}
+			if err := jpeg.Encode(buf, resizedImg, opts); err != nil {
+				return nil, err
+			}
+		default:
+			if err := png.Encode(buf, resizedImg); err != nil {
+				return nil, err
+			}
+		}
+		res_c = buf.Bytes()
+
+	} else {
+		res_c = res.Bytes()
+	}
+	return &pb.ImageDownloadReplay{Image: res_c}, err
 }
