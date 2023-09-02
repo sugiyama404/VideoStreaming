bdea5e1e20e6143e68c6f1b8c74e5b5076174bd4
sugiyama404 <mmingriha@gmail.com>
2023-09-02 11:34:57 +0900

    front view

--- webserver/myapp/app/page.tsx
+++ webserver/myapp/app/page.tsx
@@ -1,7 +1,76 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
+    <>
+      <div className="uk-child-width-1-3 uk-grid-match uk-grid uk-padding-small uk-padding-remove-vertical" uk-grid="true">
+        <div className="uk-first-column">
+          <div className="uk-card uk-card-default">
+            <div className="uk-card-media-top uk-text-center">
+              <img src="https://images.unsplash.com/photo-1490822180406-880c226c150b?fit=crop&amp;w=400&amp;h=200&amp;q=80" alt="" />
+            </div>
+            <div className="uk-card-body uk-card-small uk-padding-remove uk-margin-small-left uk-margin-small-right">
+              <div className="uk-card-title">Label</div>
+              <p className="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur nnn adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default">
+            <div className="uk-card-media-top uk-text-center">
+              <img src="https://images.unsplash.com/photo-1490822180406-880c226c150b?fit=crop&amp;w=400&amp;h=200&amp;q=80" alt="" />
+            </div>
+            <div className="uk-card-body uk-card-small uk-padding-remove uk-margin-small-left uk-margin-small-right">
+              <div className="uk-card-title">Label</div>
+              <p className="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur nnn adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default">
+            <div className="uk-card-media-top uk-text-center">
+              <img src="https://images.unsplash.com/photo-1490822180406-880c226c150b?fit=crop&amp;w=400&amp;h=200&amp;q=80" alt="" />
+            </div>
+            <div className="uk-card-body uk-card-small uk-padding-remove uk-margin-small-left uk-margin-small-right">
+              <div className="uk-card-title">Label</div>
+              <p className="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur nnn adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="uk-child-width-1-3 uk-grid-match uk-grid  uk-padding-small uk-padding-remove-vertical" uk-grid="true">
+        <div className="uk-first-column">
+          <div className="uk-card uk-card-default">
+            <div className="uk-card-media-top uk-text-center">
+              <img src="https://images.unsplash.com/photo-1490822180406-880c226c150b?fit=crop&amp;w=400&amp;h=200&amp;q=80" alt="" />
+            </div>
+            <div className="uk-card-body uk-card-small uk-padding-remove uk-margin-small-left uk-margin-small-right">
+              <div className="uk-card-title">Label</div>
+              <p className="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur nnn adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default">
+            <div className="uk-card-media-top uk-text-center">
+              <img src="https://images.unsplash.com/photo-1490822180406-880c226c150b?fit=crop&amp;w=400&amp;h=200&amp;q=80" alt="" />
+            </div>
+            <div className="uk-card-body uk-card-small uk-padding-remove uk-margin-small-left uk-margin-small-right">
+              <div className="uk-card-title">Label</div>
+              <p className="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur nnn adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default">
+            <div className="uk-card-media-top uk-text-center">
+              <img src="https://images.unsplash.com/photo-1490822180406-880c226c150b?fit=crop&amp;w=400&amp;h=200&amp;q=80" alt="" />
+            </div>
+            <div className="uk-card-body uk-card-small uk-padding-remove uk-margin-small-left uk-margin-small-right">
+              <div className="uk-card-title">Label</div>
+              <p className="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur nnn adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
