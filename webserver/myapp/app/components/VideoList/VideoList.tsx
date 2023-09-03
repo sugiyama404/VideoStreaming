0d77d049401a0bd84d4d10ea2da5393019d323af
sugiyama404 <mmingriha@gmail.com>
2023-09-03 10:27:14 +0900

    tmp saved

--- webserver/myapp/app/components/VideoList/VideoList.tsx
+++ webserver/myapp/app/components/VideoList/VideoList.tsx
@@ -1,27 +1,24 @@
 import Video from './Video';
+import { VideoHomelist } from '@/types/typing/video';
 
-export default function VideoList() {
-  const videos = [
-    { id: 1, title: 'about' },
-    { id: 2, title: 'skills' },
-    { id: 3, title: 'values' },
-    { id: 4, title: 'skills' },
-    { id: 5, title: 'values' },
-    { id: 6, title: 'future' }
-  ];
+interface VideoHomelistProps {
+  videos: VideoHomelist[];
+}
+
+export default function VideoList({ videos }: VideoHomelistProps) {
   return (
     <>
       {videos.map((video, index: number) => {
         if (index % 3 == 0) {
           return (
             <div className="uk-child-width-1-3 uk-grid-match uk-grid uk-padding-small uk-padding-remove-vertical" uk-grid="true">
-              <div className="uk-first-column" key={video.id}>
+              <div className="uk-first-column" key={index}>
                 <Video task={video} />
               </div>
-              <div key={video.id + 1}>
+              <div key={index + 1}>
                 <Video task={videos[index + 1]} />
               </div>
-              <div key={video.id + 2}>
+              <div key={index + 2}>
                 <Video task={videos[index + 2]} />
               </div>
             </div>
